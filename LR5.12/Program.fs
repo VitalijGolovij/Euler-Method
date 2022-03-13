8e04e35dd7ba8a9b8601527943304ac9b467f592
Vitalij <vitalijgl19@gmail.com>
2022-03-13 13:20:32 +0300

    Added 5.11 5.12

---
+++ LR5.12/Program.fs
@@ -0,0 +1,21 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+// See the 'F# Tutorial' project for more help.
+
+// Define a function to construct a message to print
+open System
+[<EntryPoint>]
+let main argv =
+    let answer language=
+        match language with
+        |"f#"->"Подлиза"
+        |"prolog"->"Подлиза"
+        |"1C"->"Россия - священная наша держава!"
+        |"PascalABC"->"завтра в школу"
+        |"fortran"|"cobol"->"Ловите динозавра!"
+        |_->"такого не знаю"
+
+    (Console.ReadLine>>answer>>Console.WriteLine)()
+
+    let user input (output:string->unit) chooser = output (chooser (input ()))
+    user Console.ReadLine Console.WriteLine answer
+    0 // return an integer exit code
--- LR5/Program.fs
+++ LR5/Program.fs
@@ -2,11 +2,19 @@
 // See the 'F# Tutorial' project for more help.
 
 // Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+open System
+let answer language=
+    match language with
+    |"f#"->"Подлиза"
+    |"prolog"->"Подлиза"
+    |"1C"->"Россия - священная наша держава!"
+    |"PascalABC"->"завтра в школу"
+    |"fortran"|"cobol"->"Ловите динозавра!"
+    |_->"такого не знаю"
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
+    printfn "Какой твой любимый язык?" 
+    let x:string = Console.ReadLine()
+    Console.WriteLine(answer x)
     0 // return an integer exit code
