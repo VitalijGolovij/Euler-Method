dea0dd43045ca2a99f99d19300f244a4d6cdfe35
Vitalij <vitalijgl19@gmail.com>
2022-03-14 14:26:33 +0300

    Added 5.18 Metod 2

--- LR5.18/Program.fs
+++ LR5.18/Program.fs
@@ -18,7 +18,18 @@ let Metod_1 x =
             rec_Metod_1 x (counter-1) newCur
     rec_Metod_1 x (x-1) 0
 
+let Metod_2 x =
+    let rec rec_Metod_2 x cur=
+        if x = 0 then cur 
+        else
+            let newCur = if (x%10%3<>0)&&(x%10>cur) then x%10 else cur
+            let newX= x/10
+            rec_Metod_2 newX newCur
+    rec_Metod_2 x (-1)
+
+
 [<EntryPoint>]
 let main argv =
     Console.WriteLine(Metod_1 6)
+    Console.WriteLine(Metod_2 3686)
     0 // return an integer exit code
