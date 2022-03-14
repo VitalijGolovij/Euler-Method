125bc34ec5503c9b31e2dd4b02bdc092297883fe
Vitalij <vitalijgl19@gmail.com>
2022-03-14 12:48:52 +0300

    Added 5.16

--- LR5.16/Program.fs
+++ LR5.16/Program.fs
@@ -18,9 +18,17 @@ let components x f init =
             rec_components x f (counter-1) newCur
     rec_components x f (x-1) init
 
+let Euler x =
+    let rec rec_Euler x counter cur=
+        if counter = 0 then cur 
+        else
+            let newCur = if (nod x counter)=1 then cur+1 else cur
+            rec_Euler x (counter-1) newCur
+    rec_Euler x (x-1) 0
+
 [<EntryPoint>]
 let main argv =
-    Console.WriteLine(components 5 (fun x y->x+y) 0)
+    Console.WriteLine(Euler 5)
 
     0 // return an integer exit code
     //5 4 3 2 1 =
