7cbd9ba07947ce8e19b91e4f6bec18126d5362cf
Vitalij <vitalijgl19@gmail.com>
2022-03-14 12:22:29 +0300

    Added 5.15

---
+++ LR5.15/Program.fs
@@ -0,0 +1,26 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+// See the 'F# Tutorial' project for more help.
+
+// Define a function to construct a message to print
+open System
+
+let rec nod (a:int) (b:int) =
+    if ((a % b) = 0) then 
+        b
+    else
+        nod a (a%b)
+
+let components x f init =
+    let rec rec_components x f counter cur=
+        if counter = 1 then cur 
+        else
+            let newCur = if (nod x counter)=1 then f counter cur else cur
+            rec_components x f (counter-1) newCur
+    rec_components x f (x-1) init
+
+[<EntryPoint>]
+let main argv =
+    Console.WriteLine(components 5 (fun x y->x*y) 1)
+
+    0 // return an integer exit code
+    //5 4 3 2 1 =
