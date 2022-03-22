bedc8d65d2aae0082298bba907ac7336e710c393
Vitalij <vitalijgl19@gmail.com>
2022-03-22 16:36:32 +0300

    Added Laba7.4

---
+++ Laba7/7-4/Program.fs
@@ -0,0 +1,27 @@
+﻿open System
+
+let rec readList n = 
+    if n=0 then []
+    else
+    let Head = System.Convert.ToInt32(System.Console.ReadLine())
+    let Tail = readList (n-1)
+    Head::Tail
+
+let readData = 
+    let n=System.Convert.ToInt32(System.Console.ReadLine())
+    readList n
+
+
+let task list =
+    let indexList = List.indexed list//каждому эелемнту поставим индекс (0,2) (1,8)
+    let sortListByValue = List.rev (List.sortBy (fun (x,y)->y) indexList)//сортирует список по значению
+    List.map (fun (x,y)->x) sortListByValue//возвращает список только из индексов
+        
+    
+[<EntryPoint>]
+let main argv =
+    let list = readData
+    let ans = task list
+    Console.WriteLine("ans:")
+    list|>List.iter (printfn "%i")
+    0 // return an integer exit code
