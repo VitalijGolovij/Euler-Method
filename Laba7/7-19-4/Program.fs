af44abd2e9ed9f0c43f1ed60440e7336c9b3ba90
Vitalij <vitalijgl19@gmail.com>
2022-03-31 22:34:05 +0300

    Added Laba7.19.4

---
+++ Laba7/7-19-4/Program.fs
@@ -0,0 +1,22 @@
+﻿open System
+
+let IsPalindrom (str:string) =
+    let arrChar = str.ToCharArray()//массив символов
+    let revArrChar = Array.rev arrChar//развернутый массив символов
+    let rec recIsPolindrom arr1 arr2 idx =
+        if idx+1>(Array.length arr1) then true
+        else
+            if (Array.get arr1 idx)<>(Array.get arr2 idx) then false
+            else
+                recIsPolindrom arr1 arr2 (idx+1)
+    if (recIsPolindrom arrChar revArrChar 0) then 
+        Console.WriteLine("Является")
+        else
+        Console.WriteLine("Не является")
+
+[<EntryPoint>]
+let main argv =
+    
+    let str = Console.ReadLine()
+    IsPalindrom str
+    0
