d87cf6f762d80591e838c26b469f5ea92690b1c7
Vitalij <vitalijgl19@gmail.com>
2022-04-01 19:08:11 +0300

    Added Laba7.19.15

--- Laba7/7-19-4/Program.fs
+++ Laba7/7-19-4/Program.fs
@@ -1,6 +1,7 @@
 ﻿open System
 
-let IsPalindrom (str:string) =
+let IsPalindrom =
+    let str = Console.ReadLine()
     let arrChar = str.ToCharArray()//массив символов
     let revArrChar = Array.rev arrChar//развернутый массив символов
     let rec recIsPolindrom arr1 arr2 idx =
@@ -14,7 +15,8 @@ let IsPalindrom (str:string) =
         else
         Console.WriteLine("Не является")
 
-let getCountSpace (str:string) =
+let getCountSpace =
+    let str = Console.ReadLine()
     let arrChar = str.ToCharArray()
     let rec recGetCountSpace arr idx count=
         if idx+1>(Array.length arr) then count
@@ -23,10 +25,29 @@ let getCountSpace (str:string) =
             recGetCountSpace arr (idx+1) newCount
     Console.WriteLine(recGetCountSpace arrChar 0 0)
 
+let countDefFigure =
+    let x = int (Console.ReadLine())
+    let figureArr = Array.empty
+    let rec recCountDefFigure x (arr:int array) =
+        if x=0 then arr.Length
+        else
+            let cur = x%10
+            let newArr= if (Array.tryFind (fun x->x=cur) arr)<>None then arr else Array.append arr [|cur|]
+            let newX=x/10
+            recCountDefFigure newX newArr
+    Console.WriteLine(recCountDefFigure x figureArr)
+
+let choose = function
+    | 1 -> IsPalindrom
+    | 2 -> getCountSpace
+    | _ -> countDefFigure
 
 [<EntryPoint>]
 let main argv =
-    
-    let str = Console.ReadLine()
-    getCountSpace str
+    printfn "Какую задачу решить?"
+    printfn "1. Проверка слова на палиндром"
+    printfn "2. Посчитать количество пробелов"
+    printfn "2. Количество уникальных цифр в числе"
+
+    Console.ReadLine() |> Int32.Parse |> choose
     0
