80d6a5c36c5db10a7934bd913b558d88aed623c5
Vitalij <vitalijgl19@gmail.com>
2022-04-02 12:48:46 +0300

    Fixed Laba7.19

--- Laba7/7-19-4/Program.fs
+++ Laba7/7-19-4/Program.fs
@@ -1,22 +1,10 @@
 ﻿open System
 
-let IsPalindrom =
-    let str = Console.ReadLine()
-    let arrChar = str.ToCharArray()//массив символов
-    let revArrChar = Array.rev arrChar//развернутый массив символов
-    let rec recIsPolindrom arr1 arr2 idx =
-        if idx+1>(Array.length arr1) then true
-        else
-            if (Array.get arr1 idx)<>(Array.get arr2 idx) then false
-            else
-                recIsPolindrom arr1 arr2 (idx+1)
-    if (recIsPolindrom arrChar revArrChar 0) then 
-        Console.WriteLine("Является")
-        else
-        Console.WriteLine("Не является")
+let IsPalindrom (str:string) =
+    let revStr = Seq.rev str
+    if (Seq.forall2 (fun x y -> x = y) str revStr)=false then Console.WriteLine("Не является") else Console.WriteLine("Является")
 
-let getCountSpace =
-    let str = Console.ReadLine()
+let getCountSpace (str:string) =
     let arrChar = str.ToCharArray()
     let rec recGetCountSpace arr idx count=
         if idx+1>(Array.length arr) then count
@@ -25,8 +13,8 @@ let getCountSpace =
             recGetCountSpace arr (idx+1) newCount
     Console.WriteLine(recGetCountSpace arrChar 0 0)
 
-let countDefFigure =
-    let x = int (Console.ReadLine())
+let countDefFigure (str:string) =
+    let x = Convert.ToInt32(str)
     let figureArr = Array.empty
     let rec recCountDefFigure x (arr:int array) =
         if x=0 then arr.Length
@@ -37,17 +25,18 @@ let countDefFigure =
             recCountDefFigure newX newArr
     Console.WriteLine(recCountDefFigure x figureArr)
 
-let choose = function
-    | 1 -> IsPalindrom
-    | 2 -> getCountSpace
-    | _ -> countDefFigure
-
 [<EntryPoint>]
 let main argv =
+    printfn "Введите строку:"
+    let str = Console.ReadLine()
     printfn "Какую задачу решить?"
     printfn "1. Проверка слова на палиндром"
     printfn "2. Посчитать количество пробелов"
-    printfn "2. Количество уникальных цифр в числе"
-
-    Console.ReadLine() |> Int32.Parse |> choose
+    printfn "3. Количество уникальных цифр в числе"
+    let choose = Convert.ToInt32(Console.ReadLine())
+    printf "Ответ:"
+    match choose with
+    |1->IsPalindrom str
+    |2->getCountSpace str
+    |_->countDefFigure str
     0
