dcb7bfee487b32b0aa6879cf1061c6912106b348
Vitalij <vitalijgl19@gmail.com>
2022-04-17 16:23:49 +0300

    Added Laba8.4

---
+++ Laba8/8-4/Program.fs
@@ -0,0 +1,39 @@
+﻿open System
+
+// Операторы async, let! И return! используются для асинхронного программирования. Но оно не рассмотрено в курсе :(
+
+
+let timeNow = System.DateTime.Now.ToLongTimeString()
+
+let messageResponder = MailboxProcessor.Start(fun inbox->
+    // Цикл обработки сообщений
+    let rec messageLoop() = async {
+        // Чтение сообщения
+        let! (msg:string) = inbox.Receive()
+
+        let response = match msg.ToLower() with
+        | "привет" -> "Привет"
+        | "сколько время?" -> timeNow
+        | _ -> "Пока что не понимаю"
+
+        printfn "%s" response
+
+        return! messageLoop()
+    }
+    
+    // Запуск обработки сообщений
+    messageLoop()
+)
+
+let rec askUser() =
+    let input = Console.ReadLine().Trim()
+    if not (String.IsNullOrEmpty input) then
+        messageResponder.Post(input)
+        askUser()
+
+
+[<EntryPoint>]
+let main argv =
+    printfn "Введите сообщение"
+    askUser()
+    0
