3ff92cf9d44d6d29977d80aad0e75da276c29fdb
Vitalij <vitalijgl19@gmail.com>
2022-04-25 14:11:42 +0300

    Added Laba9.8

---
+++ Laba9/9-lab8/9-lab8/Program.fs
@@ -0,0 +1,36 @@
+﻿open System
+open System.Windows.Forms
+open System.Drawing
+
+Application.EnableVisualStyles()
+// Создание формы с заголовком "Работа с массивом"
+let form = new Form(Text="Работа с массивом")
+
+let textBox1 = new TextBox(Width=150)
+let textBox2 = new TextBox(Width=150,Top=100)
+let label1 = new Label(Text="Входной массив",Top = 20)
+let label2 = new Label(Text="Выходноый массив",Top =120)
+let button1 = new Button(Text="Фильтр",Top=50)
+
+form.Controls.Add(textBox1)
+form.Controls.Add(textBox2)
+form.Controls.Add(label1)
+form.Controls.Add(label2)
+form.Controls.Add(button1)
+
+textBox1.Text<-"1,2,3,4,5,6,7,8,9,10"
+
+let myFoo (str:string) =
+    let ansArr=Array.filter (fun x->int(x)%3=0) (str.Split(','))
+    let ansStr =""
+    (ansStr,ansArr)||>Array.fold (fun a x->a+x+",") 
+
+let f _ =
+    let inptStr=textBox1.Text
+    let outStr=myFoo inptStr
+    textBox2.Text<-outStr
+
+let _ = button1.Click.Add(f)
+
+// запуск формы
+Application.Run(form)
